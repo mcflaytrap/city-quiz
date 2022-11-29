36d1eef2d17b7ab2809a05f945762821dd0dbff2
mcflaytrap <egeberk20@hotmail.com>
2022-11-29 18:35:11 +0300

    changed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import turtle
-from turtle import Turtle, Screen
 
-import pandas
 import pandas as pd
 
 screen= turtle.Screen()
@@ -11,28 +9,28 @@ screen.addshape(image)
 turtle.shape(image)
 
 provinces_data = pd.read_csv("turkish_provinces_by_plates.csv")
-provinces=provinces_data.to_list()
+provinces_data = provinces_data.rename(columns = {'X ':'X'})
 
 guessed_provinces=[]
-
+provinces_left=[]
 
 while len(guessed_provinces) < 81:
+    all_provinces = provinces_data.provinces.to_list()
+
     answer = screen.textinput(
         title=f"{len(guessed_provinces)}/Guess the names of the Turkey's 81 provinces !", prompt="What's the province's name?").title()
     if answer == "Exit":
-        provinces_left=[]
-        for province in provinces:
-            if province not in guessed_provinces:
-                provinces_left.append(province)
-        new_data =pandas.DataFrame(provinces_left)
-        new_data.to_csv("provinces_remaining.csv")
-        break
-    if answer in provinces:
+      provinces_left = list(set(all_provinces) - set(guessed_provinces))
+      new_data = pd.DataFrame(provinces_left, columns=['Province to learn'])
+      new_data.to_csv('Provinces not guessed-2.csv')
+      break
+
+    if answer in all_provinces:
         guessed_provinces.append(answer)
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
-        data = provinces_data[provinces_data == answer]
+        data = provinces_data[provinces_data.provinces == answer]
         t.goto(int(data.x), int(data.y))
         t.write(answer)
 
